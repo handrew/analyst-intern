9cb1271b65c83813abb44e3fb1ce74a015eb0d73
Andrew Han <handrew11@gmail.com>
2024-09-04 21:04:02 -0700

    tts cli

--- read.py
+++ read.py
@@ -1,7 +1,7 @@
+"""Text to speech cli."""
 import os
 import tempfile
 import click
-from pathlib import Path
 from openai import OpenAI
 from langchain_text_splitters import TokenTextSplitter
 from tqdm import tqdm
@@ -9,15 +9,17 @@ from tqdm import tqdm
 
 @click.command()
 @click.option("--fpath", default="speech.mp3", help="Output file.")
-@click.option("--text", default=None, help="Output file.")
-def cli(fpath, text):
+@click.option("--text", default=None, help="txt file or string.")
+@click.option("--voice", default="nova", help="Voice to use.")
+@click.option("--model", default="tts-1", help="Which model.")
+def cli(fpath, text, voice, model):
     """Read text."""
     assert text is not None, "Need to give text param."
     assert fpath.endswith("mp3")
 
     # Read text.
     if text.endswith("txt"):
-        with open(text, "r") as f:
+        with open(text, "r", encoding="utf-8") as f:
             text = f.read()
 
     # Split text.
@@ -26,25 +28,21 @@ def cli(fpath, text):
 
     # Text to speech.
     with tempfile.TemporaryDirectory() as tmpdir:
-        for i, text in tqdm(enumerate(texts)):
+        for i, _text in tqdm(enumerate(texts)):
             client = OpenAI()
-            response = client.audio.speech.create(
-                model="tts-1",
-                voice="nova",
-                input=text
-            )
+            response = client.audio.speech.create(model=model, voice=voice, input=_text)
             outpath = fpath.replace(".mp3", f"_{i}.mp3")
             outpath = os.path.join(tmpdir, outpath)
             response.stream_to_file(outpath)
-            print(text)
+            print(_text)
 
         # ffmpeg it together.
         cmd = f"""ffmpeg -i 'concat:{
             '|'.join([os.path.join(tmpdir, fpath.replace('.mp3', f'_{i}.mp3')) for i in range(len(texts))])
         }' -c copy {fpath}"""
         os.system(cmd)
-        
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
+
--- readme.md
+++ readme.md
@@ -1,8 +1,22 @@
 # tts-cli
 
+Install ffmpeg with homebrew.
+
+`pip install requirements.txt`
+
+Then: 
 ```
 python read.py --help
-```
-```
+
+Usage: read.py [OPTIONS]
+
+  Read text.
+
+Options:
+  --fpath TEXT  Output file.
+  --text TEXT   txt file or string.
+  --voice TEXT  Voice to use.
+  --model TEXT  Which model.
+  --help        Show this message and exit.
 ```
 
